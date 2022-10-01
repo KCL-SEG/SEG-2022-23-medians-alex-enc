ab67af0d86fdedf80072fd15ddfb9420dfd33f9d
Alexandra Encarnacion <alexandra.encarnacion@kcl.ac.uk>
2022-10-01 22:54:35 +0100

    Find median

--- median.py
+++ median.py
@@ -1,5 +1,21 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+def findMedian(numbers):
+    numbers = (sorted(numbers))
+    middle = len(numbers)//2
+    if len(numbers) % 2 == 0:
+        if numbers[middle] == numbers[middle - 1]:
+            median = numbers[middle]
+        else:
+            if numbers[middle] - numbers[middle - 1] == 1:
+                median = (numbers[middle - 1] + 0.5)
+            else:
+                median = (f'{numbers[middle - 1]}, {numbers[middle]}')
+
+    else:
+        median = numbers[middle]
+
+    return median
 
 while True:
     try:
@@ -9,4 +25,5 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+print(findMedian(numbers))
